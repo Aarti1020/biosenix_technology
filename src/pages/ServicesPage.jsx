acd07778c114a04b42c84d7d60a9914799bb10dc
Aarti Tribhuvan <tribhuvanaarti1997@gmail.com>
2026-03-15 17:55:05 +0530

    Remove unused color variable

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -1,16 +1,13 @@
 
 
-import { color } from "framer-motion";
+
 import Footer from "../components/Footer";
 import Services from "../components/Services";
 
 function ServicesPage() {
   return (
     <>
-      {/* <div className="bg-primary text-white text-center ">
-        <h1 style={{color:"purple"}}>Our Services</h1>
-        <p>We provide innovative IT solutions</p>
-      // </div> */}
+   
 
       <Services />
 
